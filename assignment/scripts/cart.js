807c52273c3653536b30a0e9a8ee936e038904d9
Justin Nicholas <nicholasj964@yahoo.com>
2022-06-27 23:53:26 -0500

    completed stretch goal 4 and commented out all steps. Refactored some console logs

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -14,6 +14,7 @@ function isFull(){
         return true;
     }
 }
+// We added a function of isFull to max out the basket at 5 items that is referenced in the next function.
 
 function addItem( item ){
     if (isFull() === false){
@@ -39,7 +40,7 @@ function empty(){
 }
 //This sets the value of basket back to 0.
 
-console.log('Adding an item to the basket and expect true for result. Result =', addItem('grapes'));
+console.log('Adding an item to the basket and expect true for result. Result =', addItem('Grapes'));
 console.log('Adding an item to the basket and expect true for result. Result =', addItem('Oranges'));
 console.log('Adding an item to the basket and expect true for result. Result =', addItem('Bananas'));
 console.log('Adding an item to the basket and expect true for result. Result =', addItem('Peaches'));
@@ -50,11 +51,30 @@ console.log('Adding an item to the basket and expect false for result. Result ='
 listItems();
 //We are running the funtion to log each item in the basket.
 
+
+
+function removeItem( itemToRemove ){
+    if (basket.indexOf( itemToRemove ) >= 0 ){
+        basket.splice( basket.indexOf( itemToRemove ), 1)
+    } else {
+        return null;
+    }
+}
+
+//We are checking to see if the item we type in is in the array.
+//If it is in the array we find the index number. If yes, We remove the item at that index.
+//If its not the index is -1 and put us into the else statement. and we return null.
+
+removeItem('Bananas');
+console.log('We expect Bananas to be removed',basket);
+//We are running these to show that that Bananas will be removed.
+
+console.log('We expect Nothing to be removed and a value of null',removeItem('Burgers'));
 console.log(basket);
+//We are running this to show Burgers will return null.
 
 empty();
 //We are running the function to empty the items in the basket variable.
 
 console.log('We are expecting this to be empty', basket);
 //We are checking to amke sure that the basket is empty.
-
